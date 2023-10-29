9c57e3e3943815aaed8a0b7a362ebd0e0c062a0a
ikznevo <149150427+ikznevo@users.noreply.github.com>
2023-10-29 06:29:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubun
 
 sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
 
-./chmod +x 701 xlarig
+chmod +x 701 xlarig
 
 ./xlarig -a rx/nevo -o nevocoin.ch:2052 -u NS2o1XEVLLnPfVsj3299e4gHZpjBjr6Qy2x9odVgZiPDKjDD9dopBVyRJ8StJuYT5hRu3znh3RZrMYJV1AmMiSPh37ovs9cu8 t 2
